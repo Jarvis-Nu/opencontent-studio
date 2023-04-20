004153b26663155d0520ab5d77eaa5def9ba831c
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-20 20:39:58 +0100

    opencontent-studio-ui

--- ui/components/TopNavigation.tsx
+++ ui/components/TopNavigation.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function TopNavigation() {
     return(
-        <div className="w-full p-5 flex justify-between items-center border-b-gray-500 border-b">
+        <div className="flex items-center justify-between w-full p-5 border-b border-b-gray-500">
             <div className="flex items-center space-x-1.5">
                 <div className="w-[45px] h-[45px] relative">
                     <Image src={"/logo.png"} layout="fill" alt="logo" />
@@ -13,7 +13,9 @@ export default function TopNavigation() {
                 </div>
             </div>
             <div>
-                <ConnectButton />
+                <button className="p-1.5 border border-white rounded-md">
+                    <ConnectButton />
+                </button>
             </div>
         </div>
     )
--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -33,7 +33,13 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+      <RainbowKitProvider chains={chains} theme={darkTheme({
+        accentColor: '#000000',
+        accentColorForeground: 'white',
+        borderRadius: 'small',
+        fontStack: 'system',
+        overlayBlur: 'small',
+      })}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
